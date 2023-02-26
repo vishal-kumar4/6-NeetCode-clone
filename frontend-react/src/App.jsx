512dcdf28eef32672a62526221492fc359c74903
vishal-kumar4 <vishal8143667512@gmail.com>
2023-02-26 15:39:36 +0530

    navbar and navbar other buttons

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button'
+
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
-    </div>
-    
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Neetcode Clone</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/courses">Courses</Nav.Link>
+          <Nav.Link href="/practice">Practice</Nav.Link>
+          <Nav.Link href="/roadmap">RoadMap</Nav.Link>
+          <Nav.Link href="/pro">Pro</Nav.Link>
+        </Nav>
+          <div>
+            <Button variant="outline-primary">
+               dark
+            </Button>
+            <Button variant="primary">
+               Sign In
+            </Button>
+          </div>
+      </Container>
+    </Navbar>
+  </div>
   )
 }
 
