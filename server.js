03c9a7bf3fb15d0f7d1b8702912a80276f40882c
vishal-kumar4 <vishal8143667512@gmail.com>
2023-02-26 14:14:53 +0530

    connecting MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const mongoose=require('mongoose')
+
+
+// ⁡⁢⁣ to make sure all key values of bulk editor or the cyclic are incorporate in our project
+// we dont give our key bcoz of if commited it'll go public , so we store in cyclic string variables and give it a name⁡
+const dotenv=require('dotenv')
+dotenv.config();
+
+//express
 const express=require('express')
 const app=express();
+
 
 // ⁡⁢⁣⁢to use static file like html css from frontend folder⁡⁡
 app.use(express.static("frontend"))
@@ -8,6 +18,23 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname + "/frontend/index.html")
 });
 
-app.listen(3000,()=>{
-    console.log(`running at port ${3000}`)
-});     
+
+// app.listen(3000,()=>{
+//     console.log(`running at port ${3000}`)
+// }); 
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,(err)=>{
+    if(err)
+    {
+        console.log(err)
+    }
+    else{
+        console.log("    - - - >   DB Connection Successful   < - - -  ")
+            // ⁡⁢⁣⁢once DB connection is successfully  only then server js starts as only after successfully connection ⁡
+        app.listen(3000,()=>{
+            console.log(` = = = = = = > running at port ${3000}  < = = = = = = `)
+        }); 
+    }
+})
